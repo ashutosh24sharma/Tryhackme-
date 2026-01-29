c9e4d35915b6f743ad984acc29f33812f70bf98f
ashutosh24sharma <151420381+ashutosh24sharma@users.noreply.github.com>
2026-01-29 21:18:42 +0530

    logo change

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -47,13 +47,11 @@ const Navbar = () => {
   }, [isOpen]);
 
   const CloudIcon = () => (
-    <svg
-      className="w-7 h-7 md:w-9 md:h-9 text-[#88cc14]"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-    >
-      <path d="M19.35 10.04C18.67 6.59 15.64 4 12 4 9.11 4 6.6 5.64 5.35 8.04 2.34 8.36 0 10.91 0 14c0 3.31 2.69 6 6 6h13c2.76 0 5-2.24 5-5 0-2.64-2.05-4.78-4.65-4.96z" />
-    </svg>
+    <img
+      src="/Logo.jpeg"
+      alt="Cloud Icon"
+      className="w-9 h-10 md:w-9 md:h-9 text-[#88cc14]"
+    />
   );
 
   const SearchIcon = () => (
